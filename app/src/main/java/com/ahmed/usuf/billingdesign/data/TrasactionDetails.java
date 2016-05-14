26b8bdea794bc128ae8fc1b79bd835b3e5a913ec
Mohammed Yousuff <myousuff@mast.com>
2016-05-14 23:49:08 +0530

    After full testing

---
+++ app/src/main/java/com/ahmed/usuf/billingdesign/data/TrasactionDetails.java
@@ -0,0 +1,16 @@
+package com.ahmed.usuf.billingdesign.data;
+
+/**
+ * Created by myousuff on 5/14/16.
+ */
+public class TrasactionDetails {
+    private int billNumber;
+
+    public int getBillNumber() {
+        return billNumber;
+    }
+
+    public void setBillNumber(int billNumber) {
+        this.billNumber = billNumber;
+    }
+}
