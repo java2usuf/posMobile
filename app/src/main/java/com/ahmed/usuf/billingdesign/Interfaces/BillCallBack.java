ba4cf14cd5f060c1b8dc9422bcd73935b3cfba57
Mohammed Yousuff <myousuff@mast.com>
2016-10-24 14:31:48 +0530

    UI Changes

--- app/src/main/java/com/ahmed/usuf/billingdesign/Interfaces/BillCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.ahmed.usuf.billingdesign.Interfaces;
-
-/**
- * Created by Ahmed-Mariam on 5/26/2016.
- */
-public interface BillCallBack {
-    void printBill();
-}
--- app/src/main/java/com/ahmed/usuf/billingdesign/Interfaces/fragmentLifeCycle.java
+++
@@ -1,9 +0,0 @@
-package com.ahmed.usuf.billingdesign.Interfaces;
-
-/**
- * Created by Ahmed-Mariam on 5/13/2016.
- */
-public interface fragmentLifeCycle {
-    public void onPauseFragment();
-    public void onResumeFragment();
-}
